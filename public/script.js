a0be271680544d87c6215ded2d45598cff70afad
victor.amigos111@gmail.com <Victor Fernandes>
2026-05-09 18:20:02 -0300

    catálogo feito

---
+++ public/script.js
@@ -0,0 +1,103 @@
+const catalogo = [
+
+    {
+        id: 1,
+        titulo: "Interestelar",
+        tipo: "filme",
+        ano: 2014,
+        generos: ["ficção científica", "drama"],
+        nota: 9.2,
+        assistido: true
+    },
+
+    {
+        id: 2,
+        titulo: "Breaking Bad",
+        tipo: "serie",
+        ano: 2008,
+        generos: ["drama", "crime"],
+        nota: 9.8,
+        assistido: true
+    },
+
+    {
+        id: 3,
+        titulo: "The Batman",
+        tipo: "filme",
+        ano: 2022,
+        generos: ["ação"],
+        nota: 8.4,
+        assistido: false
+    },
+
+    {
+        id: 4,
+        titulo: "Dark",
+        tipo: "serie",
+        ano: 2017,
+        generos: ["ficção científica", "mistério"],
+        nota: 8.9,
+        assistido: true
+    },
+
+    {
+        id: 5,
+        titulo: "Duna",
+        tipo: "filme",
+        ano: 2021,
+        generos: ["aventura", "ficção científica"],
+        nota: 8.1,
+        assistido: false
+    },
+
+    {
+        id: 6,
+        titulo: "Arcane",
+        tipo: "serie",
+        ano: 2021,
+        generos: ["animação", "ação"],
+        nota: 9.1,
+        assistido: false
+    },
+
+    {
+        id: 7,
+        titulo: "The Walking Dead",
+        tipo: "serie",
+        ano: 2010,
+        generos: ["drama", "terror"],
+        nota: 8.1,
+        assistido: true
+    },
+
+    {
+        id: 8,
+        titulo: "Michael",
+        tipo: "filme",
+        ano: 2026,
+        generos: ["biografia", "drama"],
+        nota: 7.7,
+        assistido: false
+    },
+
+    {
+        id: 9,
+        titulo: "Round 6",
+        tipo: "serie",
+        ano: 2021,
+        generos: ["suspense", "drama"],
+        nota: 4.2,
+        assistido: true
+    },
+
+    {
+        id: 10,
+        titulo: "Prision Break",
+        tipo: "serie",
+        ano: 2005,
+        generos: ["ação", "crime"],
+        nota: 8.3,
+        assistido: true
+    }
+
+];
